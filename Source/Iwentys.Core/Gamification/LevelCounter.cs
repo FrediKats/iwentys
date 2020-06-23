c7aad1b57649660ec47dd7611a87465ff5269e9c
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-06-23 23:34:25 +0300

    feat: add level counter

---
+++ Source/Iwentys.Core/Gamification/LevelCounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Iwentys.Core.Gamification
+{
+    public class LevelCounter
+    {
+        private const int MaxLevel = 35;
+        
+        private static readonly List<int> ExperienceForLevel = Enumerable.Range(0, MaxLevel + 1).Select(ConvertLevelToExperienceBound).ToList();
+
+        public int CurrentExperience { get; }
+        public int Level => ExperienceForLevel.FindLastIndex(exp => exp <= CurrentExperience);
+        public int ExperienceToNextLevel => ExperienceForLevel[Level + 1];
+
+        public LevelCounter(int currentExperience)
+        {
+            CurrentExperience = currentExperience;
+        }
+
+        public static int ConvertLevelToExperienceBound(int level) => (int)(level * Math.Pow(2, 5.7 + level / 20.0));
+    }
+}
