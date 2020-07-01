afe608a1202426d963a5ac3b5cbefb87b22e51b8
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-07-02 02:37:08 +0300

    feat: add UserGraduationYear

---
+++ Source/Iwentys.Models/Types/UserGraduationYear.cs
@@ -0,0 +1,16 @@
+﻿namespace Iwentys.Models.Types
+{
+    public enum UserGraduationYear
+    {
+        Y20 = 1,
+        Y21,
+        Y22,
+        Y23,
+        Y24
+    }
+
+    public static class UserGraduationYearExtensions
+    {
+        public static bool IsCurrentFirstCourse(this UserGraduationYear userGraduationYear) => userGraduationYear == UserGraduationYear.Y24;
+    }
+}
