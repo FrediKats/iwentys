9d21766f80e3a65501c2f64654fa36f953d6ec36
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-07-15 15:08:51 +0300

    feat: add controller for isu auth

---
+++ Source/Iwentys.Api/Controllers/IsuAuth.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Iwentys.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IsuAuth : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int code)
+        {
+            return Ok();
+        }
+    }
+}
