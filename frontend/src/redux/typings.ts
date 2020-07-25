b98aa2b295c18e0ddbb3e08bd539b98bc64827fc
mariashipkova <mariashipkova@yandex-team.ru>
2020-07-25 22:43:43 +0300

    chore(frontend): guild typings

--- frontend/src/redux/typings.ts
+++ frontend/src/redux/typings.ts
@@ -1,4 +1,46 @@
-import {IGuildState} from "./reducers/guild";
+export interface IAchievement{
+    name: string;
+    description: string;
+    url: string;
+}
+
+export interface IStudent{
+    barsPoints: number;
+    creationTime: Date;
+    firstName: string;
+    githubUsername: string;
+    group: string;
+    id: number;
+    lastOnlineTime: Date;
+    middleName: string;
+    role: string;
+    secondName: string;
+}
+
+export interface IMemberLeaderBoard {
+    members: IStudent[];
+    membersImpact: Record<string, object> // ???
+    totalRate: string;
+}
+
+export interface IRepository {
+    id: number;
+    description: string;
+    starCount: number;
+    url: string;
+}
+
+export interface IGuildState {
+    title: string;
+    bio: string;
+    logoUrl: string;
+    tribute: number | null;
+    achievements: IAchievement[];
+    leader: IStudent;
+    totem: IStudent;
+    memberLeaderBoard: IMemberLeaderBoard;
+    pinnedRepositories: IRepository[];
+}
 
 export interface IState {
     guild: IGuildState;
