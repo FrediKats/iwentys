6b5bd4149ce222dc69403de8814408c77da35dbd
s4xack <sergmiron47@gmail.com>
2020-07-28 18:11:48 +0300

    feat: added tests for get requests and blocked

--- Source/Iwentys.Tests/Core/Services/GuildServiceTest.cs
+++ Source/Iwentys.Tests/Core/Services/GuildServiceTest.cs
@@ -83,5 +83,77 @@ namespace Iwentys.Tests.Core.Services
             Assert.IsNotEmpty(tributes);
             Assert.True(tributes.Any(t => t.ProjectId == project.Id));
         }
+
+        [Test]
+        public void GetGuildRequests_ForGuildEditorAndNoRequestsToGuild_EmptyGildMembersArray()
+        {
+            var context = TestCaseContext
+                .Case()
+                .WithNewStudent(out AuthorizedUser user)
+                .WithGuild(user, out GuildProfileDto guild);
+
+            GuildMember[] requests = context.GuildService.GetGuildRequests(user, guild.Id);
+
+            Assert.That(requests, Is.Not.Null);
+            Assert.That(requests.Any(), Is.False);
+        }
+
+        [Test]
+        public void GetGuildRequests_ForNotGuildMember_ThrowInnerLogicException()
+        {
+            var context = TestCaseContext
+                .Case()
+                .WithNewStudent(out AuthorizedUser user)
+                .WithGuild(user, out GuildProfileDto guild)
+                .WithNewStudent(out AuthorizedUser student);
+
+            Assert.Throws<InnerLogicException>(() => context.GuildService.GetGuildRequests(student, guild.Id));
+        }
+
+        [Test]
+        public void GetGuildRequests_ForGuildMemberWithoutEditorPermissions_ThrowInnerLogicException()
+        {
+            var context = TestCaseContext
+                .Case()
+                .WithNewStudent(out AuthorizedUser user)
+                .WithGuild(user, out GuildProfileDto guild)
+                .WithGuildMember(guild, out AuthorizedUser member);
+
+            Assert.Throws<InnerLogicException>(() => context.GuildService.GetGuildRequests(member, guild.Id));
+        }
+
+        [Test]
+        public void GetGuildRequests_ForGuildEditor_GildMembersArrayWithRequestStatus()
+        {
+            var context = TestCaseContext
+                .Case()
+                .WithNewStudent(out AuthorizedUser user)
+                .WithGuild(user, out GuildProfileDto guild)
+                .WithGuildRequest(guild, out AuthorizedUser student);
+
+            GuildMember[] requests = context.GuildService.GetGuildRequests(user, guild.Id);
+
+            Assert.That(requests, Is.Not.Null);
+            Assert.That(requests.Length, Is.EqualTo(1));
+            Assert.That(requests[0].MemberId, Is.EqualTo(student.Id));
+            Assert.That(requests[0].MemberType, Is.EqualTo(GuildMemberType.Requested));
+        }
+
+        [Test]
+        public void GetGuildBlocked_ForGuildEditor_GildMembersArrayWithBlockedStatus()
+        {
+            var context = TestCaseContext
+                .Case()
+                .WithNewStudent(out AuthorizedUser user)
+                .WithGuild(user, out GuildProfileDto guild)
+                .WithGuildBlocked(guild, out AuthorizedUser student);
+
+            GuildMember[] requests = context.GuildService.GetGuildBlocked(user, guild.Id);
+
+            Assert.That(requests, Is.Not.Null);
+            Assert.That(requests.Length, Is.EqualTo(1));
+            Assert.That(requests[0].MemberId, Is.EqualTo(student.Id));
+            Assert.That(requests[0].MemberType, Is.EqualTo(GuildMemberType.Blocked));
+        }
     }
 }
