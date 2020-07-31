82c0b66e73da39865b754d538fc59632b8c485ec
Vasilii Ivanov <iwanovvasilij@yandex.ru>
2020-08-01 02:12:02 +0300

    feat: add dummy data seeding for SubjectForGroup

--- Source/Iwentys.Database/Context/IwentysDbContext.cs
+++ Source/Iwentys.Database/Context/IwentysDbContext.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using System.Linq;
 using Iwentys.Models.Entities;
 using Iwentys.Models.Entities.Guilds;
 using Iwentys.Models.Entities.Study;
+using Iwentys.Models.Types;
 using Microsoft.EntityFrameworkCore;
 
 namespace Iwentys.Database.Context
@@ -50,6 +52,12 @@ namespace Iwentys.Database.Context
             modelBuilder.Entity<GuildMember>().HasIndex(g => g.MemberId).IsUnique();
             modelBuilder.Entity<CompanyWorker>().HasIndex(g => g.WorkerId).IsUnique();
 
+            modelBuilder.Entity<StudyProgram>().HasData(getStudyProgramsList());
+            modelBuilder.Entity<StudyGroup>().HasData(getStudyGroupsList());
+            modelBuilder.Entity<Teacher>().HasData(getTeachersList());
+            modelBuilder.Entity<Subject>().HasData(getSubjectsList());
+            modelBuilder.Entity<SubjectForGroup>().HasData(getSubjectForGroupsList());
+
             //TODO: fix
             var cascadeFKs = modelBuilder.Model.GetEntityTypes()
                 .SelectMany(t => t.GetForeignKeys())
@@ -66,6 +74,181 @@ namespace Iwentys.Database.Context
             modelBuilder.Entity<GuildMember>().HasKey(g => new {g.GuildId, g.MemberId});
             modelBuilder.Entity<CompanyWorker>().HasKey(g => new {g.CompanyId, g.WorkerId});
             modelBuilder.Entity<SubjectActivity>().HasKey(s => new {s.SubjectForGroupId, s.StudentId});
+        }
+
+        /// <summary>
+        /// Следующие 5 методов - методы вызываемые при создании базы для того,
+        /// чтобы внести в нее данные о группах, направлениях и т.д.
+        /// Это сделано из расчета на то, что такая информация будет редко меняться и
+        /// по этому ее не нужно получать через API.
+        /// TODO: Нужно создать конфиг файл для каждого подобного набора данных и получать данные оттуда, а не заполнять прямо в коде
+        /// 
+        /// </summary>
+        /// <returns>Список объектов, которые будут помещены в базу при загрузке</returns>
+        private List<StudyProgram> getStudyProgramsList()
+        {
+            var result = new List<StudyProgram> {new StudyProgram {Id = 1, Name = "ИС"}};
+
+            return result;
+        }
+        private List<StudyGroup> getStudyGroupsList()
+        {
+            var result = new List<StudyGroup>
+            {
+                new StudyGroup
+                {
+                    NamePattern = "М3201", StudyProgram = new StudyProgram {Id = 1, Name = "ИС"}, Year = 2020
+                },
+                new StudyGroup
+                {
+                    NamePattern = "М3202", StudyProgram = new StudyProgram {Id = 2, Name = "ИС"}, Year = 2020
+                },
+                new StudyGroup
+                {
+                    NamePattern = "М3203", StudyProgram = new StudyProgram {Id = 3, Name = "ИС"}, Year = 2020
+                }
+            };
+
+            return result;
+        }
+
+        private List<Subject> getSubjectsList()
+        {
+            var result = new List<Subject>
+            {
+                new Subject {Id = 1, Name = "Programming"}, new Subject {Id = 2, Name = "Physical Culture"}
+            };
+
+            return result;
+        }
+
+        private List<Teacher> getTeachersList()
+        {
+            var result = new List<Teacher>
+            {
+                new Teacher {Id = 1, Name = "Жмышенко Валерий Альбертович"},
+                new Teacher {Id = 2, Name = "Сухачев Денис Владимирович"}
+            };
+
+            return result;
+        }
+
+        private List<SubjectForGroup> getSubjectForGroupsList()
+        {
+            var result = new List<SubjectForGroup>
+            {
+                new SubjectForGroup
+                {
+                    Id = 1,
+                    SubjectId = 1,
+                    Subject = new Subject {Id = 1, Name = "Programming"},
+                    StudyGroupId = 1,
+                    StudyGroup =
+                        new StudyGroup
+                        {
+                            Id = 1,
+                            NamePattern = "М3201",
+                            StudyProgram = new StudyProgram {Id = 1, Name = "ИС"},
+                            Year = 2020
+                        },
+                    LecturerId = 2,
+                    Lecturer = new Teacher {Id = 2, Name = "Сухачев Денис Владимирович"},
+                    StudySemester = StudySemester.Y20H1
+                },
+                new SubjectForGroup
+                {
+                    Id = 2,
+                    SubjectId = 2,
+                    Subject = new Subject {Id = 2, Name = "Physical Culture"},
+                    StudyGroupId = 1,
+                    StudyGroup =
+                        new StudyGroup
+                        {
+                            Id = 1,
+                            NamePattern = "М3201",
+                            StudyProgram = new StudyProgram {Id = 1, Name = "ИС"},
+                            Year = 2020
+                        },
+                    LecturerId = 1,
+                    Lecturer = new Teacher {Id = 1, Name = "Жмышенко Валерий Альбертович"},
+                    StudySemester = StudySemester.Y20H1
+                },
+                new SubjectForGroup
+                {
+                    Id = 3,
+                    SubjectId = 1,
+                    Subject = new Subject {Id = 1, Name = "Programming"},
+                    StudyGroupId = 1,
+                    StudyGroup =
+                        new StudyGroup
+                        {
+                            Id = 2,
+                            NamePattern = "М3202",
+                            StudyProgram = new StudyProgram {Id = 1, Name = "ИС"},
+                            Year = 2020
+                        },
+                    LecturerId = 2,
+                    Lecturer = new Teacher {Id = 2, Name = "Сухачев Денис Владимирович"},
+                    StudySemester = StudySemester.Y20H1
+                },
+                new SubjectForGroup
+                {
+                    Id = 4,
+                    SubjectId = 2,
+                    Subject = new Subject {Id = 2, Name = "Physical Culture"},
+                    StudyGroupId = 1,
+                    StudyGroup =
+                        new StudyGroup
+                        {
+                            Id = 2,
+                            NamePattern = "М3202",
+                            StudyProgram = new StudyProgram {Id = 1, Name = "ИС"},
+                            Year = 2020
+                        },
+                    LecturerId = 1,
+                    Lecturer = new Teacher {Id = 1, Name = "Жмышенко Валерий Альбертович"},
+                    StudySemester = StudySemester.Y20H1
+                },
+                new SubjectForGroup
+                {
+                    Id = 5,
+                    SubjectId = 1,
+                    Subject = new Subject {Id = 1, Name = "Programming"},
+                    StudyGroupId = 1,
+                    StudyGroup =
+                        new StudyGroup
+                        {
+                            Id = 3,
+                            NamePattern = "М3203",
+                            StudyProgram = new StudyProgram {Id = 1, Name = "ИС"},
+                            Year = 2020
+                        },
+                    LecturerId = 2,
+                    Lecturer = new Teacher {Id = 2, Name = "Сухачев Денис Владимирович"},
+                    StudySemester = StudySemester.Y20H1
+                },
+                new SubjectForGroup
+                {
+                    Id = 6,
+                    SubjectId = 2,
+                    Subject = new Subject {Id = 2, Name = "Physical Culture"},
+                    StudyGroupId = 1,
+                    StudyGroup =
+                        new StudyGroup
+                        {
+                            Id = 3,
+                            NamePattern = "М3203",
+                            StudyProgram = new StudyProgram {Id = 1, Name = "ИС"},
+                            Year = 2020
+                        },
+                    LecturerId = 1,
+                    Lecturer = new Teacher {Id = 1, Name = "Жмышенко Валерий Альбертович"},
+                    StudySemester = StudySemester.Y20H1
+                }
+            };
+
+
+            return result;
         }
     }
 }
