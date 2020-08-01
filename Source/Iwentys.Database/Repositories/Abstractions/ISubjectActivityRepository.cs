6fa0a9ee27a94e8b5c582014ff2a28620df42b30
Vasilii Ivanov <iwanovvasilij@yandex.ru>
2020-08-01 04:23:55 +0300

    feat: add repositories for SubjectActivity and SubjectForGroup

---
+++ Source/Iwentys.Database/Repositories/Abstractions/ISubjectActivityRepository.cs
@@ -0,0 +1,8 @@
+﻿using Iwentys.Models.Entities.Study;
+
+namespace Iwentys.Database.Repositories.Abstractions
+{
+    public interface ISubjectActivityRepository : IGenericRepository<SubjectActivity, int>
+    {
+    }
+}
