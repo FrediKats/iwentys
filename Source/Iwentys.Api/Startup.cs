852a823d8116ec11037390d469d4af429339b23d
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-08-04 01:34:14 +0300

    fix: add missed namespace

--- Source/Iwentys.Api/Startup.cs
+++ Source/Iwentys.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Iwentys.Api
 {
