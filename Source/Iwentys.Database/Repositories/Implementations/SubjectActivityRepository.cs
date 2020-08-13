2e99afec82844cedc17d7386166b56f7814b6c6a
s4xack <sergmiron47@gmail.com>
2020-08-14 01:35:11 +0300

    refactor: change line wrap

--- Source/Iwentys.Database/Repositories/Implementations/SubjectActivityRepository.cs
+++ Source/Iwentys.Database/Repositories/Implementations/SubjectActivityRepository.cs
@@ -54,10 +54,11 @@ namespace Iwentys.Database.Repositories.Implementations
 
         public IEnumerable<SubjectActivity> GetStudentActivities(StudySearchDto searchDto)
         {
-            var query = Read().Join(_dbContext.StudyGroups, st => st.Student.GroupId, sg => sg.Id, 
-                (subjectActivity, group) => new {SubjectActivity = subjectActivity, Group = group}).Join(
-                _dbContext.SubjectForGroups, st => st.Group.Id, sg => sg.StudyGroupId,
-                (_, sg) => new {_.SubjectActivity, _.Group, SubjectForGroup = sg});
+            var query = Read()
+                .Join(_dbContext.StudyGroups, st => st.Student.GroupId, sg => sg.Id, 
+                    (subjectActivity, group) => new {SubjectActivity = subjectActivity, Group = group})
+                .Join(_dbContext.SubjectForGroups, st => st.Group.Id, sg => sg.StudyGroupId,
+                    (_, sg) => new {_.SubjectActivity, _.Group, SubjectForGroup = sg});
 
             if (searchDto.GroupId != null)
             {
