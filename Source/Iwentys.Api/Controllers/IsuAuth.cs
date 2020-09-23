267640fc0f9e09ddd36e75d5e369002295d470e4
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-09-24 02:35:10 +0300

    fix: auth controller spec miss match resolved

--- Source/Iwentys.Api/Controllers/IsuAuth.cs
+++ Source/Iwentys.Api/Controllers/IsuAuth.cs
@@ -5,20 +5,20 @@ namespace Iwentys.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class IsuAuth : ControllerBase
+    public class IsuAuthController : ControllerBase
     {
-        private readonly ILogger<IsuAuth> _logger;
+        private readonly ILogger<IsuAuthController> _logger;
 
-        public IsuAuth(ILogger<IsuAuth> logger)
+        public IsuAuthController(ILogger<IsuAuthController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IActionResult Get(int code)
+        public ActionResult<string> Get(string code)
         {
             _logger.LogInformation($"Get code for isu auth: {code}");
-            return Ok();
+            return Ok(code);
         }
     }
 }
