7420f2132a7c560527464bfc2e875cfcc4acb857
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-09-27 21:56:19 +0300

    fix: add loading info about github on profile

--- Source/Iwentys.Models/Entities/Github/ContributionFullInfo.cs
+++ Source/Iwentys.Models/Entities/Github/ContributionFullInfo.cs
@@ -7,7 +7,6 @@ namespace Iwentys.Models.Entities.Github
 {
     public class ContributionFullInfo
     {
-        public int Id { get; set; }
         public ActivityInfo RawActivity { get; set; }
         public int Total => PerMonthActivity().Sum(a => a.Count);
 
--- Source/Iwentys.Models/Entities/GithubUserData.cs
+++ Source/Iwentys.Models/Entities/GithubUserData.cs
@@ -1,5 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+
 using Iwentys.Models.Entities.Github;
+using Iwentys.Models.Tools;
+
+using Newtonsoft.Json;
 
 namespace Iwentys.Models.Entities
 {
@@ -7,10 +11,18 @@ namespace Iwentys.Models.Entities
     {
         [Key]
         public int StudentId { get; set; }
+        public StudentEntity Student { get; set; }
+
         public string Username { get; set; }
         public string AvatarUrl { get; set; }
         public string Bio { get; set; }
         public string Company { get; set; }
-        public ContributionFullInfo ContributionFullInfo { get; set; }
+        public string SerializedContributionData { get; set; }
+
+        public ContributionFullInfo ContributionFullInfo
+        {
+            get => SerializedContributionData.Maybe(JsonConvert.DeserializeObject<ContributionFullInfo>);
+            set => SerializedContributionData = JsonConvert.SerializeObject(value);
+        }
     }
 }
