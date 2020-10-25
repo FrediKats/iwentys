9253e83faf80faf11f2e2072f78f50d4930c7302
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-10-25 13:14:19 +0300

    feat: add controller for schedule

---
+++ Source/Iwentys.Api/Controllers/ScheduleController.cs
@@ -0,0 +1,20 @@
+﻿using ItmoScheduleApiWrapper.Models;
+using Iwentys.Core.Services.Implementations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Iwentys.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        private ScheduleService _scheduleService = new ScheduleService();
+
+        [HttpGet("today/{group}")]
+        public ActionResult<ScheduleItemModel> GetTodaySchedule(string group)
+        {
+            //TODO: group validation
+            return Ok(_scheduleService.GetForGroup(group));
+        }
+    }
+}
