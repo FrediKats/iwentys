78df888ba6f981484e27e31b14da8c7d06e65419
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-11-09 00:05:13 +0300

    feat: add project with wasm

---
+++ Source/Endpoints/Iwentys.Endpoint/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer4.EntityFramework.Options;
+
+using Iwentys.Endpoint.Server.Models;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Iwentys.Endpoint.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
