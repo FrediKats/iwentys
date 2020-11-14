d6dc8204f7e9061ed056aba352eb26e62ea9fe20
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2020-11-14 06:46:16 +0300

    feat: add test method for getting token, client wrapper for auth controller

--- Source/Endpoints/Iwentys.Endpoint.Client/Program.cs
+++ Source/Endpoints/Iwentys.Endpoint.Client/Program.cs
@@ -1,33 +1,35 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Iwentys.Endpoint.Client.Tools;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Iwentys.Endpoint.Client
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("Iwentys.Endpoint.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            builder.Services.AddHttpClient("Iwentys.Endpoint.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<IAuthService, AuthService>();
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Iwentys.Endpoint.ServerAPI"));
 
             builder.Services.AddApiAuthorization();
 
-            await builder.Build().RunAsync();
+            return builder.Build().RunAsync();
         }
     }
 }
---
+++ Source/Endpoints/Iwentys.Endpoint.Client/Tools/AuthService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Iwentys.Endpoint.Sdk.ControllerClients;
+using Iwentys.Models.Transferable;
+
+namespace Iwentys.Endpoint.Client.Tools
+{
+    public interface IAuthService
+    {
+        Task<IwentysAuthResponse> Login(int userId);
+    }
+
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthService(HttpClient httpClient,
+            ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+
+        public async Task<IwentysAuthResponse> Login(int userId)
+        {
+            IwentysAuthResponse iwentysAuthResponse = await new AuthControllerClient(_httpClient).Login(userId);
+
+            await _localStorage.SetItemAsync("authToken", iwentysAuthResponse.Token);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", iwentysAuthResponse.Token);
+
+            return iwentysAuthResponse;
+        }
+    }
+}
