24c7d5f1856af4dba49b5488e9ca6a90eb6c8bd4
Fredi Kats <inredikawb@gmail.com>
2020-12-21 01:04:03 +0300

    feat: add quest revoking

--- Source/Endpoints/Iwentys.Endpoint.Client/Pages/Quests/QuestJournalPage.razor.cs
+++ Source/Endpoints/Iwentys.Endpoint.Client/Pages/Quests/QuestJournalPage.razor.cs
@@ -2,19 +2,28 @@
 using System.Threading.Tasks;
 using Iwentys.Endpoint.Client.Tools;
 using Iwentys.Endpoint.Sdk.ControllerClients;
+using Iwentys.Endpoint.Sdk.ControllerClients.Study;
+using Iwentys.Features.Quests.Enums;
 using Iwentys.Features.Quests.Models;
+using Iwentys.Features.Students.Models;
 
 namespace Iwentys.Endpoint.Client.Pages.Quests
 {
     public partial class QuestJournalPage
     {
         private IReadOnlyList<QuestInfoDto> _selectedQuest;
+        private StudentInfoDto _currentStudent;
 
+        private StudentControllerClient _studentControllerClient;
         private QuestControllerClient _questControllerClient;
 
         protected override async Task OnInitializedAsync()
         {
-            _questControllerClient = new QuestControllerClient(await Http.TrySetHeader(LocalStorage));
+            var httpClient = await Http.TrySetHeader(LocalStorage);
+            _studentControllerClient = new StudentControllerClient(httpClient);
+            _questControllerClient = new QuestControllerClient(httpClient);
+
+            _currentStudent = await _studentControllerClient.GetSelf();
         }
         
         private async Task SelectActive()
@@ -33,6 +42,17 @@ namespace Iwentys.Endpoint.Client.Pages.Quests
         {
             _selectedQuest = await _questControllerClient.GetArchived();
             StateHasChanged();
+        }
+
+        private bool IsQuestCanBeRevoked(QuestInfoDto quest)
+        {
+            return quest.State == QuestState.Active && quest.Author.Id == _currentStudent?.Id;
+        }
+        
+        private async Task RevokeQuest(QuestInfoDto quest)
+        {
+            //TODO: refresh elements
+            await _questControllerClient.Revoke(quest.Id, _currentStudent.Id);
         }
 
         private string LinkToQuestProfilePage(QuestInfoDto quest) => $"/quest/profile/{quest.Id}";
