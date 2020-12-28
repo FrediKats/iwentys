962154134af347cc61a02b6eed6d9e965244a65f
Fredi Kats <inredikawb@gmail.com>
2020-12-28 10:37:12 +0300

    feat: add tournamens list page

---
+++ Source/Endpoints/Iwentys.Endpoint.Client/Pages/Guilds/Tournaments/TournamentJournalPage.razor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Iwentys.Endpoint.Client.Tools;
+using Iwentys.Endpoint.Sdk.ControllerClients.Guilds;
+using Iwentys.Features.Guilds.Tournaments.Models;
+
+namespace Iwentys.Endpoint.Client.Pages.Guilds.Tournaments
+{
+    public partial class TournamentJournalPage
+    {
+        private GuildControllerClient _guildControllerClient;
+        private GuildTributeControllerClient _guildTributeControllerControllerClient;
+        private TournamentControllerClient _tournamentControllerClient;
+
+        private List<TournamentInfoResponse> _tournaments;
+
+        protected override async Task OnInitializedAsync()
+        {
+            HttpClient httpClient = await Http.TrySetHeader(LocalStorage);
+            _guildControllerClient = new GuildControllerClient(httpClient);
+            _guildTributeControllerControllerClient = new GuildTributeControllerClient(httpClient);
+            _tournamentControllerClient = new TournamentControllerClient(httpClient);
+
+            _tournaments = await _tournamentControllerClient.Get();
+        }
+    }
+}
---
+++ Source/Endpoints/Iwentys.Endpoint.Sdk/ControllerClients/Guilds/TournamentControllerClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Iwentys.Features.Guilds.Tournaments.Models;
+
+namespace Iwentys.Endpoint.Sdk.ControllerClients.Guilds
+{
+    public class TournamentControllerClient
+    {
+        public TournamentControllerClient(HttpClient client)
+        {
+            Client = client;
+        }
+
+        public HttpClient Client { get; }
+
+        public Task<List<TournamentInfoResponse>> Get()
+        {
+            return Client.GetFromJsonAsync<List<TournamentInfoResponse>>($"/api/tournaments");
+        }
+
+        public Task<TournamentInfoResponse> Get(int tournamentId)
+        {
+            return Client.GetFromJsonAsync<TournamentInfoResponse>($"/api/tournaments/{tournamentId}");
+        }
+    }
+}
