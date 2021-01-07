39ff5e7987917fec4409781a97f529cfc739af09
Fredi Kats <inredikawb@gmail.com>
2021-01-07 14:24:50 +0300

    refactor: rename guilds components and page

--- Source/Endpoints/Iwentys.Endpoint.Client/Pages/Guilds/GuildPinnedRepositoriesComponent.razor.cs
+++ Source/Endpoints/Iwentys.Endpoint.Client/Pages/Guilds/GuildPinnedRepositoriesComponent.razor.cs
@@ -2,7 +2,7 @@
 
 namespace Iwentys.Endpoint.Client.Pages.Guilds
 {
-    public partial class GuildProfilePinnedRepositoriesComponent
+    public partial class GuildPinnedRepositoriesComponent
     {
         public async Task RemovePin(long repositoryId)
         {
