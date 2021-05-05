b7b7209aaa6460834cdcf2cc16f6454f89b307f4
Fredi Kats <fredikats@outlook.com>
2021-05-05 23:04:54 +0300

    refactor: reorganize project structure

---
+++ Source/Infrastructure/Iwentys.Application/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+using Iwentys.Features.Extended.Services;
+using Iwentys.Features.Gamification.Karmas;
+using Iwentys.Features.Gamification.Services;
+using Iwentys.Features.GithubIntegration.GithubIntegration;
+using Iwentys.Features.Guilds.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Iwentys.Features.Guilds.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIwentysGuildFeatureServices(this IServiceCollection services)
+        {
+            services.AddScoped<GuildMemberService>();
+            services.AddScoped<GuildService>();
+            services.AddScoped<GuildTestTaskService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddIwentysTributesFeatureServices(this IServiceCollection services)
+        {
+            services.AddScoped<GuildTributeService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddIwentysGithubIntegrationFeatureServices(this IServiceCollection services)
+        {
+            services.AddScoped<GithubIntegrationService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddIwentysGamificationFeatureServices(this IServiceCollection services)
+        {
+            services.AddScoped<StudyLeaderboardService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddIwentysEconomyFeatureServices(this IServiceCollection services)
+        {
+            services.AddScoped<BarsPointTransactionLogService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddIwentysQuestFeatureServices(this IServiceCollection services)
+        {
+            services.AddScoped<QuestService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddIwentysRaidFeatureServices(this IServiceCollection services)
+        {
+            services.AddScoped<RaidService>();
+
+            return services;
+        }
+    }
+}
