7a575e17d262ba8432171ed579bd0bde75210be3
Fredi Kats <fredikats@outlook.com>
2021-05-18 19:20:16 +0300

    feat: generate identity pages

---
+++ Source/Endpoints/Iwentys.Endpoints.Api/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Iwentys.Endpoints.Api.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Iwentys.Endpoints.Api.Areas.Identity.IdentityHostingStartup))]
+namespace Iwentys.Endpoints.Api.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
