05d1962938ea574a11ebdc835b40a726e758058c
Bibletoon <alex8.ivanov@gmail.com>
2021-07-25 11:07:39 +0300

    Feat: add assigments view mode

--- Source/Endpoints/Iwentys.Endpoints.WebClient/Modules/SubjectAssignments/StudentPages/StudentSubjectAssignmentPage.razor.cs
+++ Source/Endpoints/Iwentys.Endpoints.WebClient/Modules/SubjectAssignments/StudentPages/StudentSubjectAssignmentPage.razor.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Iwentys.Sdk;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Iwentys.Endpoints.WebClient.Modules.SubjectAssignments.StudentPages
 {
     public partial class StudentSubjectAssignmentPage
     {
+        private enum ViewMode
+        {
+            All,
+            Completed,
+            Uncompleted
+        }
+        
         private ICollection<SubjectAssignmentDto> _subjectAssignments;
         private ICollection<SubjectAssignmentSubmitDto> _subjectAssignmentSubmits;
-
+        private ViewMode _currentViewMode = ViewMode.Uncompleted;
+        private StudentInfoDto _self;
+        
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
 
+            _self = await StudentClient.GetSelfAsync();
             _subjectAssignments = await _studentSubjectAssignmentClient.GetStudentSubjectAssignmentsAsync(SubjectId);
             _subjectAssignmentSubmits = await _studentSubjectAssignmentClient.GetStudentSubjectAssignmentSubmitsAsync(SubjectId);
+        }
+
+        private bool subjectAssignmentSatisfyViewMode(SubjectAssignmentDto subjectAssignment)
+        {
+            return (_currentViewMode == ViewMode.All
+                    || (_currentViewMode == ViewMode.Completed &&
+                        subjectAssignment.Submits.Any(
+                            submit => submit.Student.Id == _self.Id && (submit.State == SubmitState.Approved))
+                        || (_currentViewMode == ViewMode.Uncompleted && !subjectAssignment.Submits.Any(
+                            submit => submit.Student.Id == _self.Id && (submit.State == SubmitState.Approved)))
+                    ));
         }
 
         private string LinkToCreateSubmit() => $"/subject/{SubjectId}/assignments/create-submit";
