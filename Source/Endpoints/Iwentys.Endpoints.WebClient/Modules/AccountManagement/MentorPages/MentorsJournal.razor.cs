6c219f4f9e079ce411a0a5247222ad4dc2738ebe
Bibletoon <alex8.ivanov@gmail.com>
2021-08-27 22:31:18 +0300

    Feat: add hide not my subjects checkbox

--- Source/Endpoints/Iwentys.Endpoints.WebClient/Modules/AccountManagement/MentorPages/MentorsJournal.razor.cs
+++ Source/Endpoints/Iwentys.Endpoints.WebClient/Modules/AccountManagement/MentorPages/MentorsJournal.razor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading.Tasks;
 using Iwentys.Sdk;
 using MudBlazor;
@@ -8,15 +10,46 @@ namespace Iwentys.Endpoints.WebClient.Modules.AccountManagement.MentorPages
 {
     public partial class MentorsJournal
     {
-        private ICollection<SubjectMentorsDto> _subjectsMentors;
+        private ICollection<SubjectMentorsDto> _allSubjectsMentors;
+        private ICollection<SubjectMentorsDto> _subjectsMentorsToShow;
+
+        private bool _showOnlyMySubjects;
+
+        public bool ShowOnlyMySubjects
+        {
+            get => _showOnlyMySubjects;
+
+            set
+            {
+                _showOnlyMySubjects = value;
+                if (!_showOnlyMySubjects)
+                {
+                    _subjectsMentorsToShow = _allSubjectsMentors;
+                }
+                else
+                {
+                    _subjectsMentorsToShow = _allSubjectsMentors.Where(
+                        sm => sm.Groups.Any(g => g.LectorMentor.Id == _currentUser.Id
+                                                 || g.PracticeMentors.Any(m => m.Id == _currentUser.Id))).ToList();
+                }
+            }
+        }
+
+        private StudentInfoDto _currentUser;
         
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
-            _subjectsMentors = await MentorsManagementClient.GetAllAsync();
+            _currentUser = await StudentClient.GetSelfAsync();
+            _allSubjectsMentors = await MentorsManagementClient.GetAllAsync();
+            _subjectsMentorsToShow = _allSubjectsMentors;
         }
 
         public void RemoveMentor(MudChip chip)
+        {
+        }
+
+        public void ViewModeChanged()
         {
             
         }
