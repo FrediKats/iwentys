15362c5b1e73059c2b3d54ebd791a4fa71b942a7
Bibletoon <alex8.ivanov@gmail.com>
2021-08-27 21:41:21 +0300

    Feat: add mentors view ui

--- Source/Endpoints/Iwentys.Endpoints.WebClient/Modules/AccountManagement/MentorPages/MentorsJournal.razor.cs
+++ Source/Endpoints/Iwentys.Endpoints.WebClient/Modules/AccountManagement/MentorPages/MentorsJournal.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Iwentys.Sdk;
 
@@ -10,6 +11,7 @@ namespace Iwentys.Endpoints.WebClient.Modules.AccountManagement.MentorPages
         
         protected override async Task OnInitializedAsync()
         {
+            await base.OnInitializedAsync();
             _subjectsMentors = await MentorsManagementClient.GetAllAsync();
         }
     }
