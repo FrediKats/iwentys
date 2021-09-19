6074fc13a72bcc65257b1500cb183deb41aa9181
Fredi Kats <fredikats@outlook.com>
2021-09-19 15:24:30 +0300

    chore: add project for account management service

---
+++ Source/AccountManagement/Iwentys.AccountManagement.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Options;
+using Iwentys.AccountManagement.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Iwentys.AccountManagement.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ Source/AccountManagement/Iwentys.AccountManagement.Server/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Iwentys.AccountManagement.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
