40e365b7b0189138dd0110200c6615bce89a802b
Bibletoon <alex8.ivanov@gmail.com>
2021-10-20 21:31:26 +0300

    Fix: add subject assignment to mediatr

---
+++ Source/Modules/Iwentys.Modules.SubjectAssignments/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Iwentys.Modules.SubjectAssignments.MentorScope;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Iwentys.Modules.SubjectAssignments
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSubjectAssignmentsModule(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(MentorSubjectAssignmentController).Assembly);
+
+            services.AddControllers().PartManager.ApplicationParts.Add(new AssemblyPart(typeof(MentorSubjectAssignmentController).Assembly));
+
+            return services;
+        }
+    }
+}
