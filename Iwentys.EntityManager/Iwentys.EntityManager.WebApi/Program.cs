5324cb6278975b8c77885116d5560fa2137e8645
Fredi Kats <fredikats@outlook.com>
2022-01-21 14:19:52 +0300

    feat: setup web api

--- Iwentys.EntityManager/Iwentys.EntityManager.WebApi/Program.cs
+++ Iwentys.EntityManager/Iwentys.EntityManager.WebApi/Program.cs
@@ -1,19 +1,39 @@
+using System.Text.Json.Serialization;
+using Iwentys.EntityManager.DataAccess;
+using Iwentys.EntityManager.WebApi;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<IwentysEntityManagerDbContext>(o => o
+    .UseLazyLoadingProxies()
+    .UseInMemoryDatabase("InMemoryIwentysEntityManager.db"));
+
+builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionPipeline<,>));
+builder.Services.AddScoped<DbContext, IwentysEntityManagerDbContext>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    using (IServiceScope serviceScope = app.Services.CreateScope())
+    {
+        var context = serviceScope.ServiceProvider.GetRequiredService<IwentysEntityManagerDbContext>();
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+    
 }
 
 app.UseHttpsRedirection();
