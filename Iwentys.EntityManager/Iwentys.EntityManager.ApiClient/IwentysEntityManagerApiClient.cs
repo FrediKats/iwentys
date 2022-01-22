61ad2301536d5c050d19844f7581e6f5cdc77817
Fredi Kats <fredikats@outlook.com>
2022-01-22 09:51:14 +0300

    feat: add IwentysEntityManagerApiClient for aggregation http clients

---
+++ Iwentys.EntityManager/Iwentys.EntityManager.ApiClient/IwentysEntityManagerApiClient.cs
@@ -0,0 +1,19 @@
+﻿namespace Iwentys.EntityManager.ApiClient;
+
+public class IwentysEntityManagerApiClient
+{
+    public GroupSubjectClient GroupSubjects { get; set; }
+    public StudentProfileClient StudentProfiles { get; set; }
+    public StudyCourseClient StudyCourses { get; set; }
+    public StudyGroupClient StudyGroups { get; set; }
+    public SubjectClient Subjects { get; set; }
+
+    public IwentysEntityManagerApiClient(HttpClient httpClient)
+    {
+        GroupSubjects = new GroupSubjectClient(httpClient);
+        StudentProfiles = new StudentProfileClient(httpClient);
+        StudyCourses = new StudyCourseClient(httpClient);
+        StudyGroups = new StudyGroupClient(httpClient);
+        Subjects = new SubjectClient(httpClient);
+    }
+}
